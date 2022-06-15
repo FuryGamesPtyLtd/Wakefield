152f4f325619d1717fe0e75c658ba797adbc191c
Andrew Tomazos <andrewtomazos@gmail.com>
2022-06-16 00:37:04 +1000

    Add Sha256

---
+++ Source/Wakefield/Public/Wakefield/Sha256.h
@@ -0,0 +1,23 @@
+﻿// © 2022 Fury Games Pty Ltd.  All rights reserved.
+
+#pragma once
+
+#include <array>
+
+#include "CoreMinimal.h"
+
+#define SHA256_BYTES_SIZE 32
+
+class WAKEFIELD_API FSha256 {
+public:
+	FSha256();
+
+	void Append(const void *data, size_t n_bytes);
+
+	std::array<uint64, 4> Finalize();
+	
+	uint32_t State[8];
+	uint8_t Buffer[64];
+	uint64_t NBits;
+	uint8_t BufferCounter;
+};
