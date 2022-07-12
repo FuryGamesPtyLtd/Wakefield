46c21842d7d4a77630e4c14efef696e0399bf15d
Andrew Tomazos <andrewtomazos@gmail.com>
2022-07-13 00:41:44 +1000

    Add EnumToDisplayName function.

---
+++ Source/Wakefield/Public/Wakefield/Enum.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <type_traits>
+
+#include "CoreMinimal.h"
+#include "Wakefield/Enstring.h"
+
+template<typename EEnum>
+FString EnumToDisplayName(const EEnum Enumerator)
+{
+	const UEnum* const EnumClass = StaticEnum<EEnum>();
+	
+	const auto Value = static_cast<std::underlying_type_t<EEnum>>(Enumerator);
+
+	if (!EnumClass)
+		return Enstring(static_cast<int64>(Value));
+	
+	FString DisplayName = EnumClass->GetDisplayNameTextByValue(Value).ToString();
+
+	if (!DisplayName.IsEmpty())
+		return DisplayName;
+	
+	DisplayName = EnumClass->GetNameStringByValue(Value);
+
+	if (!DisplayName.IsEmpty())
+		return DisplayName;
+
+	return Enstring(static_cast<int64>(Value));
+}
