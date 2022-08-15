be19eb1d8bffc45e68be29bddb4dbbe07d14bc8a
Andrew Tomazos <andrewtomazos@gmail.com>
2022-08-16 04:14:35 +1000

    Create Widgets.h

---
+++ Source/Wakefield/Public/Wakefield/Widgets.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+
+// WKF_FIND_OBJECT(Class, Variable, Path) uses an FObjectFinder to find
+// an object of type Class at Path and assigns it to Variable if successful.
+//
+#define WKF_FIND_OBJECT(Class, Variable, Path)                                         \
+	do                                                                                 \
+	{                                                                                  \
+		static ConstructorHelpers::FObjectFinder<Class> Variable##_Finder(TEXT(Path)); \
+		if (Variable##_Finder.Succeeded())                                             \
+			Variable = Variable##_Finder.Object;                                       \
+	}                                                                                  \
+	while (0)
