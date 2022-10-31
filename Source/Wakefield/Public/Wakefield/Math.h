0777ecaca792200ed034a5d1397b784190a78c3e
Andrew Tomazos <andrewtomazos@gmail.com>
2022-10-31 13:19:04 +1100

    Add unit user-defined literals

--- Source/Wakefield/Public/Wakefield/Math.h
+++ Source/Wakefield/Public/Wakefield/Math.h
@@ -20,3 +20,23 @@ constexpr FORCEINLINE int64 SignedMod(int64 A, int64 B)
 {
 	return A - B * SignedDiv(A, B);
 }
+
+constexpr double operator"" _m(unsigned long long int M)
+{
+	return M * 100.0;
+} 
+
+constexpr double operator"" _m(long double M)
+{
+	return M * 100.0;
+} 
+
+constexpr double operator"" _km(unsigned long long int Km)
+{
+	return Km * 100000.0;
+} 
+
+constexpr double operator"" _km(long double Km)
+{
+	return Km * 100000.0;
+} 
