0c122d7049fcd29ad91f463f0d62c414f92373fa
Andrew Tomazos <andrewtomazos@gmail.com>
2022-12-15 16:12:58 +1100

    Additional math functions

---
+++ Source/Wakefield/Private/Wakefield/Math.cpp
@@ -0,0 +1,25 @@
+﻿#include "Wakefield/Math.h"
+
+FVector2D NearestPointOnLineSegment(FVector2D Point, FVector2D LineA, FVector2D LineB)
+{
+	const FVector2D LineOffset = LineB - LineA;
+	const double LineLength = LineOffset.Length(); 
+	if (LineLength < KINDA_SMALL_NUMBER)
+		return LineA;
+	const FVector2D LineDirection = LineOffset.GetSafeNormal();
+
+	const FVector2D OffsetFromA = Point - LineA;
+
+	const double ProjPoint = OffsetFromA.Dot(LineDirection);
+	if (ProjPoint < 0)
+		return LineA;
+	else if (ProjPoint > LineLength)
+		return LineB;
+	else
+		return LineA + ProjPoint * LineDirection;
+}
+
+double DistanceToLineSegment(FVector2D Point, FVector2D LineA, FVector2D LineB)
+{
+	return (Point - NearestPointOnLineSegment(Point, LineA, LineB)).Length();
+}
--- Source/Wakefield/Public/Wakefield/Math.h
+++ Source/Wakefield/Public/Wakefield/Math.h
@@ -40,3 +40,45 @@ constexpr double operator"" _km(long double Km)
 {
 	return Km * 100000.0;
 }
+
+class FYawAngle
+{
+public:
+	explicit FYawAngle(double YawIn)
+	{
+		Yaw = NormalizeYaw(YawIn);
+	}
+
+	double Get() const { return Yaw; }
+
+	static double GetDelta(FYawAngle Source, FYawAngle Destination)
+	{
+		const double A = Source.Yaw;
+		const double B = Destination.Yaw;
+		double C = NormalizeYaw(B - A);
+		if (C > 180)
+			C -= 360;
+		return C;
+	}
+
+	FVector2D Sincos() const
+	{
+		FVector2D Result;
+		FMath::SinCos(&Result.Y, &Result.X, FMath::DegreesToRadians(Yaw));
+		return Result;
+	}
+
+private:
+	
+	static double NormalizeYaw(double Yaw)
+	{
+		Yaw -= 360 * std::floor(Yaw / 360.0);
+		return Yaw;
+	}
+
+	double Yaw;
+};
+
+WAKEFIELD_API FVector2D NearestPointOnLineSegment(FVector2D Point, FVector2D LineA, FVector2D LineB);
+
+WAKEFIELD_API double DistanceToLineSegment(FVector2D Point, FVector2D LineA, FVector2D LineB);
